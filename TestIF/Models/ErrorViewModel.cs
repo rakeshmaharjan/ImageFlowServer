6bef60c6492a39b53bc1e22397fc59140604fe57
rakeshmaharjan <rakeshmaharjan066@gmail.com>
2020-09-13 10:28:48 +0545

    add new project

---
+++ TestIF/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestIF.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
